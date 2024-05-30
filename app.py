e6f3d277a608178bd585f9ac0d3e6c3d9bca6e30
nixiz0 <97995554+nixiz0@users.noreply.github.com>
2024-05-30 21:10:51 +0200

    Update the App

--- app.py
+++ app.py
@@ -1,8 +1,24 @@
 import cv2
 import torch
+import tkinter as tk
+from tkinter import simpledialog
 from torchvision.transforms import ToTensor
+
+from functions.app_design import app_theme
 from functions.model_emotion_classifier import EmotionClassifier
 
+
+# Create a tkinter root window
+root = tk.Tk()
+root.withdraw()
+app_theme(root)
+
+# Ask the user for the camera number
+camera_number = None
+while camera_number is None:
+    dialogue = simpledialog.askstring("Input", "Please enter your camera number (if you only have 1, then it's 0 because indexing starts from 0)", parent=root)
+    if dialogue.isdigit():
+        camera_number = int(dialogue)
 
 # Create a dictionary to map class numbers to emotion names
 class_to_emotion = {0: 'angry', 1: 'disgust', 2: 'fear', 3: 'happy', 4: 'neutral', 5: 'sad', 6: 'surprise'}
@@ -20,7 +36,7 @@ model.eval()
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
 # Open camera
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(camera_number)
 
 while True:
     ret, frame = cap.read() # Read image from camera
@@ -33,8 +49,11 @@ while True:
     # Detect faces in image
     faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
 
-    # If no face is detected, continue the loop
+    # If no face is detected, show the image and continue the loop
     if len(faces) == 0:
+        cv2.imshow('Emotion Detection (space to exit)', frame)
+        if cv2.waitKey(1) & 0xFF == ord(' '):
+            break
         continue
 
     # Otherwise, select the largest face
---
+++ functions/app_design.py
@@ -0,0 +1,3 @@
+# Design for the simpledialog (user input)
+def app_theme(root):
+    root.tk_setPalette(background='#2b2d42', foreground='#ffffff')
